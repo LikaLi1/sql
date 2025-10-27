b71a57981c9e9339b152017f49b91564f4dd7d93
ElizaBeth <167125218+LikaLi1@users.noreply.github.com>
2025-10-27 14:12:46 +0300

    pr1.sql

--- pr1.sql
+++ pr1.sql
@@ -17,10 +17,19 @@ INSERT INTO employees (first_name, last_name, department, salary, hire_date, ema
 VALUES
 ('Petr', 'Petrov', 'IT', 45000, '2025-09-25', 'petr@ya.ru'),
 ('Beth', 'Mischenko', 'IT', 10000000, '2023-10-13', 'bet@ya.ru'),
-('Lusine', 'Kazarian', 'IT', 10000000, '1956-02-08', 'lusic@mail.ru'),
-('Denis', 'Litvinov', 'IT', 10000000, '2014-04-04', 'den4k@google.com'),
+('Lusine', 'Kazarian', 'HR', 10000000, '1956-02-08', 'lusic@mail.ru'),
+('Denis', 'Litvinov', 'HR', 10000000, '2014-04-04', 'den4k@google.com'),
 ('Katia', 'Petrova', 'IT', 35000, '2023-12-22', 'KAte@mail.ru');
 
 UPDATE employees
 SET salary = 80000
 WHERE id = 5;
+
+UPDATE employees
+SET salary = 85000
+WHERE department = 'IT';
+
+UPDATE employees
+SET salary = salary * 1.1
+WHERE department = 'HR';
+
