e7981886ae80daf736a135f293316c8ec8d883a3
ElizaBeth <167125218+LikaLi1@users.noreply.github.com>
2025-10-27 14:05:42 +0300

    pr1.sql

--- pr1.sql
+++ pr1.sql
@@ -12,3 +12,15 @@ CREATE TABLE IF NOT EXISTS employees(
     hire_date TEXT NOT NULL,
     email TEXT UNIQUE
 );
+
+INSERT INTO employees (first_name, last_name, department, salary, hire_date, email)
+VALUES
+('Petr', 'Petrov', 'IT', 45000, '2025-09-25', 'petr@ya.ru'),
+('Beth', 'Mischenko', 'IT', 10000000, '2023-10-13', 'bet@ya.ru'),
+('Lusine', 'Kazarian', 'IT', 10000000, '1956-02-08', 'lusic@mail.ru'),
+('Denis', 'Litvinov', 'IT', 10000000, '2014-04-04', 'den4k@google.com'),
+('Katia', 'Petrova', 'IT', 35000, '2023-12-22', 'KAte@mail.ru');
+
+UPDATE employees
+SET salary = 80000
+WHERE id = 5;
