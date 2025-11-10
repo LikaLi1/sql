412dbab2551f2212b739546ac46bbc934f69af39
ElizaBeth <167125218+LikaLi1@users.noreply.github.com>
2025-11-10 12:26:08 +0300

    pr2.sql

---
+++ pr2.sql
@@ -0,0 +1,18 @@
+CREATE TABLE employess (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    department TEXT NULL,
+    salary REAL NOT NULL,
+    hire_date TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL
+);
+
+INSERT INTO employess VALUES
+(1, 'IVAN', 'IVANOV', 'IT', 50000 , '2025-05-11', 'ivan@ya.ru'),
+(2, 'OLGA', 'IVANOVA', 'HR', 45000 , '2024-10-12', 'ola@rambler.ru'),
+(3, 'ALEXEY', 'PETROV', 'IT', 50000 , '2024-12-12', 'petrov@gmail.ru'),
+(4, 'MICHAIL', 'DUSHKIN', 'Sales', 40000 , '2025-09-30', 'miha@ya.ru'),
+(5, 'TATIANA', 'PETROV', 'Sales', 40000 , '2025-09-27', 'tata@inbox.ru');
+
+UPDATE employees SET salary = 85000 WHERE department = 'IT';
